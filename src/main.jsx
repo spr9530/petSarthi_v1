6890f5b77e4753e880ed821f550cc57e45613d4a
Sparsh <sparshmanhas111@gamil.com>
2025-03-05 00:15:43 +0530

    Doctor Accounts, Doctor Invoicec, Basic Setting of Doctor

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,11 @@ import DoctorLanding from './components/DoctorLanding.jsx';
 import DoctorAppointment from './components/DoctorAppointment.jsx';
 import DoctorTimings from './components/DoctorTimings.jsx';
 import DoctorReview from './components/DoctorReview.jsx';
+import DoctorAccounts from './components/DoctorAccounts.jsx';
+import DoctorInvoices from './components/DoctorInvoices.jsx';
+import DoctorChat from './components/DoctorChat.jsx';
+import SettingLayout from './doctor/SettingLayout.jsx';
+import DoctorBasicDetails from './components/DoctorBasicDetails.jsx';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
@@ -26,13 +31,21 @@ createRoot(document.getElementById('root')).render(
       </Route>
       <Route element={<DoctorLayout />}>
         <Route path='doctors' >
-          {/* Login path index */}
           <Route path='dashboard' element={<DoctorLanding />} />
           <Route path='appointments' element={<DoctorAppointment />} />
           <Route path='timings' element={<DoctorTimings />} />
           <Route path='reviews' element={<DoctorReview />} />
+          <Route path='accounts' element={<DoctorAccounts />} />
+          <Route path='invoices' element={<DoctorInvoices />} />
+          <Route element={<SettingLayout />}>
+            <Route path="settings" element={<DoctorBasicDetails />}/>
+          </Route>
         </Route>
-        </Route>
+      </Route>
+      <Route path="doctors/chat" element={<DoctorChat />}>
+
+
+      </Route>
     </Routes>
 
   </BrowserRouter>
